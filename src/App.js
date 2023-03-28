3b03fdb610f14ccb2612c3d58ba22b3591d5e086
Dawid Mądrzyk <maddawid@gmail.com>
2023-03-28 23:55:45 +0200

    btc and eth display fetched price

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
+import btclogo from './btc.svg'
+import ethlogo from './eth.svg'
+
 import './App.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+
+  const [btcPrice, setbtcPrice] = useState(null)
+  const [ethPrice, setethPrice] = useState(null)
+
+  const fetchBitcoinPrice = async () => {
+    try {
+      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd', {
+      })
+      const data = await response.json();
+      setbtcPrice(data.bitcoin.usd)
+      console.log('Current Bitcoin price (USD):', data.bitcoin.usd);
+    } catch (error) {
+      console.error('Error fetching Bitcoin price:', error);
+    }
+  };
+
+  const fetchEtherPrice = async () => {
+    try {
+      const response = await fetch('https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd', {
+      })
+      const data = await response.json();
+      setethPrice(data.ethereum.usd)
+      console.log('Current Ethereum price (USD):', data.ethereum.usd);
+    } catch (error) {
+      console.error('Error fetching Ethereum price:', error);
+    }
+  };
+  
+  useEffect(() => {
+    fetchBitcoinPrice()
+    fetchEtherPrice()
+    }, []
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    btcPrice ? 
+    <div className='App'>
+      <div className='card'>{`$ ${btcPrice}`} <img className='btcimage' src={btclogo} style={{ height: 50, width: 50 }} /></div>
+      <div className='card'>{`$ ${ethPrice}`} <img className='btcimage' src={ethlogo} style={{ height: 50, width: 50 }} /></div>
     </div>
+    : <h3>waiting</h3>
   );
 }
 
